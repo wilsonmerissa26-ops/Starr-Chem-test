3204586d9226345ebbdf82d0ee890477a9ccf924
wilsonmerissa26-ops <wilsonmerissa26@gmail.com>
2026-08-18 00:16:41 -0400

    Retire v13 from live composition regression

--- test-guided-problem-tutor-v13.js
+++ test-guided-problem-tutor-v13.js
@@ -1,6 +1,6 @@
 'use strict';
 var fs=require('fs');var js=fs.readFileSync('day1/guided-problem-tutor-v13.js','utf8');var html=fs.readFileSync('day1/index.html','utf8');var p=0,f=0;function ok(n,c){if(c){console.log('PASS  '+n);p++;}else{console.log('FAIL  '+n);f++;}}
-ok('v13 loaded after prior teaching layers',html.indexOf('guided-problem-tutor-v13.js')>html.indexOf('voice-quality-v12.js'));
+ok('legacy v13 tutor is retained but not loaded by canonical live Day 1',html.indexOf('guided-problem-tutor-v13.js')<0&&js.length>0);
 ok('help keeps same problem',js.indexOf('I am not changing the problem')>=0&&js.indexOf('Same problem tutoring')>=0);
 ok('walkthrough requires learner answers',js.indexOf('Check this step')>=0&&js.indexOf('Try this same step again')>=0);
 ok('does not reveal on first help click',js.indexOf('Walk me through it')>=0&&js.indexOf('Give me the first step only')>=0);
